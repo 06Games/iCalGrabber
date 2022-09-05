e3f5930eb4268e78aec37cb8120858f1f1d755ca
EvanG <evan_g@orange.fr>
2022-09-05 21:16:55 +0200

    Remove docker SSL

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-if (!app.Environment.IsDevelopment()) builder.WebHost.UseUrls("http://0.0.0.0:80", "https://0.0.0.0:443");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
